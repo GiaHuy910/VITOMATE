85deb6dcb9ac5d8a02e5aa67ce9ff950b7deba2d
chuyenvung666-web <chuyenvung666@gmail.com>
2026-08-13 22:30:47 +0700

    add 'platform, agent, infrastructure' direction and edit .gitinore file

--- server/src/config/db/index.js
+++ server/src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     await mongoose.connect("mongodb://localhost:27017/VITOMATE");
-    // console.log("connect sucessfully");
+    console.log("connect sucessfully");
   } catch (error) {
     console.error("Connect failed:", error);
   }
