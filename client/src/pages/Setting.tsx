8e8785b2547a096527496b72991a40f2490ae096
GiaHuy910 <nguyenbui.09101999@gmail.com>
2026-08-15 20:40:29 +0700

    added raw profile, setting page

---
+++ client/src/pages/Setting.tsx
@@ -0,0 +1,21 @@
+import { useAuth } from "../contexts/useAuth";
+
+const Setting = () => {
+  const { user, loading } = useAuth();
+
+  return (
+    <div className="flex-basic-between">
+      <h1>SETTING</h1>
+
+      {loading ? (
+        <p>Loading...</p>
+      ) : user ? (
+        <h2>Welcome, {user.username}</h2>
+      ) : (
+        <p>You have not signed in yet!</p>
+      )}
+    </div>
+  );
+};
+
+export default Setting;
